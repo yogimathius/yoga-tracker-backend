c467ae5a345674b4e3b3a94dbb21618bd2e9bb17
Mathius <info@yogimathius.dev>
2024-09-05 23:56:51 -0700

    refactor: update session post request

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -8,16 +8,50 @@ router.post("/", async (req, res) => {
   const { week, duration, progress } = req.body;
 
   try {
-    const session = await prisma.session.create({
-      data: {
-        week,
-        duration,
-        progress,
-        date: new Date(),
-      },
+    const user = await prisma.user.findUnique({
+      where: { id: 1 },
     });
 
-    return res.status(201).json({ message: "Session added", session });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (progress === 7) {
+      const result = await prisma.user.update({
+        where: { id: 1 },
+        data: {
+          currentWeek: user.currentWeek + 1,
+          currentWeekProgress: 0,
+          completedWeeks: user.completedWeeks + 1,
+          sessions: {
+            create: {
+              week,
+              duration,
+              progress,
+              date: new Date(),
+            },
+          },
+        },
+      });
+      return res.json("Session added successfully");
+    } else {
+      const result = await prisma.user.update({
+        where: { id: 1 },
+        data: {
+          currentWeekProgress: user.currentWeekProgress + 1,
+          sessions: {
+            create: {
+              week,
+              duration,
+              progress,
+              date: new Date(),
+            },
+          },
+        },
+      });
+
+      return res.json("Session added successfully");
+    }
   } catch (error) {
     return res.status(500).send({ error: "Failed to add session" });
   }
